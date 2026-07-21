2918a099d9d61949677210aaaaef11a6749ead0b
Radhika <radhikabansal365@gmail.com>
2026-07-21 14:06:08 +0530

    Backend setup

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import cors from "cors";
+import app from "./src/app.js";
+import { connectDb } from "./src/config/db.js";
+
+connectDb();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server is running at 3000");
+});
---
+++ backend/src/models/product.model.js
@@ -0,0 +1,92 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    type: {
+      type: String,
+      required: true,
+      enum: ["flowers", "cakes", "gifts", "plants", "combos"],
+    },
+
+    category: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    images: [
+      {
+        type: String,
+      },
+    ],
+
+    price: {
+      type: Number,
+      required: true,
+    },
+
+    oldPrice: {
+      type: Number,
+      default: 0,
+    },
+
+    stock: {
+      type: Number,
+      default: 0,
+    },
+
+    rating: {
+      type: Number,
+      default: 0,
+    },
+
+    reviews: {
+      type: Number,
+      default: 0,
+    },
+
+    badge: {
+      type: String,
+      enum: ["Bestseller", "Trending", "New Arrival", ""],
+      default: "",
+    },
+
+    isFeatured: {
+      type: Boolean,
+      default: false,
+    },
+
+    isBestSeller: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const productModel =
+  mongoose.models.Product || mongoose.model("Product", productSchema);
+
+export default productModel;

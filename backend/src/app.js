8c7f3bf124fd8dcc38f76bb37aeba3c519bb7b62
Radhika <radhikabansal365@gmail.com>
2026-07-22 17:03:02 +0530

    error resolved

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://flowera-ecommerce.vercel.app/"],
+    origin: ["http://localhost:5173", "https://flowera-ecommerce.vercel.app"],
     credentials: true,
   }),
 );
